e3b81334732a7342dd5e9c6ee15f34278939f2cf
Your Name <you@example.com>
2025-07-24 17:58:53 +0200

    restyle homepage and aboutus

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,24 +1,15 @@
 export default function AboutUsPage() {
     return (
         <div className="container my-5">
-            <h1 className="mb-4 text-center">About us</h1>
+            <h1 className="mb-4 text-center">Chi siamo?</h1>
+            <p className="text-center">UN NEGOZIO FINTO!</p>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, dolorem voluptate doloremque molestiae totam incidunt reprehenderit quibusdam accusamus voluptatum nulla assumenda. Repellendus dolore fugit, deleniti rem tempora aspernatur sint quod et blanditiis velit iste! Adipisci dignissimos doloribus assumenda laboriosam quibusdam in praesentium repellendus totam? Aliquid voluptatibus laboriosam accusantium totam sint.
             </p>
 
-            <h3 className="text-center py-2">THE BEST SHOP EVER</h3>
-            <ul>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                <li>Donec ullamcorper nulla non metus auctor fringilla.</li>
-                <li>Cras justo odio, dapibus ac facilisis in, egestas eget quam.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-            </ul>
-            <p>
-                Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-            </p>
+            <h3 className="text-center py-2">Non so chi siano questi tipi</h3>
+            <img src="https://yeswework.it/wp-content/uploads/2023/07/teamwork-yww.1065x600.force0_.q80.s0.zc1_.zcpC_.jpg" alt="" />
+
         </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,26 +2,20 @@ export default function HomePage() {
 
     return (
         <div className="container my-5 text-center">
-            <h1 className="mb-4">Benvenuti nel mio negozio 🛍️</h1>
-            <p className="lead mb-4">
+            <h1 className="my-5">Benvenuti nel mio negozio 🛍️</h1>
+            <p className="lead my-4 ">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nihil delectus quod, voluptates aperiam ad libero
                 unde iusto eligendi quidem temporibus quasi in. Molestiae deleniti eaque dolores voluptatum distinctio, amet
                 recusandae laudantium vel ducimus culpa dolor commodi, aliquid ea veniam earum perspiciatis laborum quos
                 deserunt temporibus libero nihil numquam necessitatibus!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nihil delectus quod, voluptates aperiam ad libero
-                unde iusto eligendi quidem temporibus quasi in. Molestiae deleniti eaque dolores voluptatum distinctio, amet
-                recusandae laudantium vel ducimus culpa dolor commodi, aliquid ea veniam earum perspiciatis laborum quos
-                deserunt temporibus libero nihil numquam necessitatibus!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nihil delectus quod, voluptates aperiam ad libero
-                unde iusto eligendi quidem temporibus quasi in. Molestiae deleniti eaque dolores voluptatum distinctio, amet
-                recusandae laudantium vel ducimus culpa dolor commodi, aliquid ea veniam earum perspiciatis laborum quos
-                deserunt temporibus libero nihil numquam necessitatibus!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nihil delectus quod, voluptates aperiam ad libero
-                unde iusto eligendi quidem temporibus quasi in. Molestiae deleniti eaque dolores voluptatum distinctio, amet
-                recusandae laudantium vel ducimus culpa dolor commodi, aliquid ea veniam earum perspiciatis laborum quos
-                deserunt temporibus libero nihil numquam necessitatibus!
-            </p>
 
+
+            </p>
+            <button className="btn btn-primary px-4 py-2">
+                Scopri di più
+            </button>
+            <p className="pt-3 fw-bold">No scherzo non premere</p>
+            <img src="https://flawless.life/wp-content/uploads/2016/02/antonia-store-2.jpg" alt="shop" />
         </div>
     )
 }
