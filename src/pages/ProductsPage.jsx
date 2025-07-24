90077ce12bc9089bd0668b375b702afbdcbb3470
Your Name <you@example.com>
2025-07-24 17:42:24 +0200

    Add Products page with data fetch and product display

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react"
+
 export default function ProductsPage() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
-        <div>
-            <h1>Prodotti</h1>
-            <p>Benvenuto nella pagina dei prodotti!</p>
-        </div>
+        <div className="container my-5">
+            <h1 className="my-5 text-center">I nostri prodotti</h1>
+            <div className="row">
+                {products.map(product => (
+                    <div key={product.id} className="col-md-4 mb-4">
+                        <div className="card h-100 shadow-sm border-0">
+
+                            <div className="text-center p-3 h-50">
+                                <img
+                                    src={product.image}
+                                    alt={product.title}
+                                    className="img-fluid h-100"
+                                />
+                            </div>
+                            <p className="mb-2 text-center">
+                                {product.category}
+                            </p>
+                            <div className="card-body d-flex flex-column">
+                                <h6 className="card-title" title={product.title}>
+                                    {product.title}
+                                </h6>
+                                <p className="card-text text-muted small mb-3">
+                                    {product.description}
+                                </p>
+                                <p className="mb-2 fw-bold">
+                                    ⭐ {product.rating.rate}
+                                </p>
+                                <p className="mt-auto fw-bold text-danger">
+                                    €{product.price}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div >
     )
 }
