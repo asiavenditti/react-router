2b2f16825b269933a90a5ff719d75185da5ddef2
Your Name <you@example.com>
2025-07-25 17:29:07 +0200

    stylize navigation buttons for next and previous product on the SingleProduct page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -32,34 +32,45 @@ export default function SingleProduct() {
         navigate(`/product/${previousPost}`)
     }
     return (
+        <div className="container my-5 d-flex justify-content-center">
+            <button
+                onClick={pre}
+                className="btn btn-danger me-3 align-self-center"
+                style={{ height: '40px' }}
+                aria-label="Prodotto precedente">
+                <i className="bi bi-arrow-left"></i>
+            </button>
 
-        <div className="container my-5 b">
-            <button onClick={pre}>
-                Precedente
-            </button >
-            <button onClick={next}>
-                Successivo
-            </button >
-            <div className="card shadow-sm border-0 mx-auto" style={{ maxWidth: "600px" }}>
-                <div className="text-center p-4" style={{ height: "300px" }}>
+
+            <div className="card shadow-sm border-0" style={{ maxWidth: "650px" }}>
+                <div className="text-center p-4" style={{ height: "400px" }}>
                     <img
                         src={product.image}
                         alt={product.title}
                         className="img-fluid h-100"
-                        style={{ objectFit: "contain" }}
-                    />
+                        style={{ objectFit: "contain" }} />
                 </div>
 
                 <p className="mb-2 text-center text-uppercase text-muted">{product.category}</p>
 
                 <div className="card-body d-flex flex-column">
-                    <h5 className="card-title" title={product.title}>{product.title}</h5>
+                    <h5 className="card-title" title={product.title}>
+                        {product.title}
+                    </h5>
                     <p className="card-text text-muted small mb-3">{product.description}</p>
-
                     <p className="mb-2 fw-bold">⭐ {product.rating?.rate}</p>
                     <p className="mt-auto fw-bold text-danger">€{Number(product.price).toFixed(2)}</p>
                 </div>
             </div>
+            <button
+                onClick={next}
+                className="btn btn-danger ms-3 align-self-center"
+                style={{ height: '40px' }}
+                aria-label="Prodotto successivo">
+                <i className="bi bi-arrow-right"></i>
+            </button>
         </div>
-    );
+
+
+    )
 }
